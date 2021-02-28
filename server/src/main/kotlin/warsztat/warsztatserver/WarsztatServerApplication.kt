a328503506cdc45ee9182c30c25bfd4f542b0b1c
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2021-02-28 13:23:31 +0100

    Spring initializer: projekt serwera

---
+++ server/src/main/kotlin/warsztat/warsztatserver/WarsztatServerApplication.kt
@@ -0,0 +1,11 @@
+package warsztat.warsztatserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WarsztatServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<WarsztatServerApplication>(*args)
+}
---
+++ server/src/test/kotlin/warsztat/warsztatserver/WarsztatServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package warsztat.warsztatserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WarsztatServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
